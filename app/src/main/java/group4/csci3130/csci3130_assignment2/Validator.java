e85b490ad32419bfec8948209ce6983f8e0044b9
Dawson Wilson <dw666766@dal.ca>
2018-06-09 13:21:36 -0300

    push with the empty functions

--- app/src/main/java/group4/csci3130/csci3130_assignment2/Validator.java
+++ app/src/main/java/group4/csci3130/csci3130_assignment2/Validator.java
@@ -1,4 +1,20 @@
 package group4.csci3130.csci3130_assignment2;
 
 public class Validator {
+    public String password;
+    public Validator(String inPassword) {
+        password = inPassword;
+    }
+
+    public boolean validatePassword(String inPassword) {
+        return true;
+    }
+
+    private boolean atLeast8Chars(String inPassword) {
+        return true;
+    }
+
+    private boolean checkIfPassword(String inPassword) {
+        return true;
+    }
 }
